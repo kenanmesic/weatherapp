17082d3490fa768a542192f1a32fcc2c07fb5786
kenan <kenanmesic1191@gmail.com>
2023-01-24 14:20:06 -0800

    added link and screenshot to readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-a weather application with a 5 day forcast
+a weather application with a 5 day forcast. temp, wind , and humidity
+
+
+link: https://kenanmesic.github.io/weatherapp/
+
+
+
+Screenshot:
+Screenshot_20230124_021835.png
--- script.js
+++ script.js
@@ -1,147 +1,25 @@
-var apiKey = "202f7bf441b9d45eeccbdc991cdc2249";
-var searchForm = document.getElementById("search-form");
-var historyEl = document.getElementById("search-history");
-var cityName = "";
-
-var cityList = [];
-var apiUrl = "https://api.openweathermap.org/data/2.5/"
-// API endpoint based on a city name, converts to lat/lon data
-function getCoordinates() {
-    var city = searchForm.children[1].value
-    console.log(apiUrl)
-    fetch(apiUrl + "weather?q=" + city + "&appid=" + apiKey)
-        .then(function (response) {
-            // request was not successful
-            if (!response.ok) {
+var searchBtn = document.getElementById("submit-button");
+var searchBarInput = document.querySelector(".searchbarInput");
+var name = document.querySelector('.name');
+var temp = document.querySelector('.temp');
+var wind = document.querySelector('.wind');
+var humidity = document.querySelector('.humidity');
+var apiKey = "1fb41e48b657cf2ffc887ad1435cafe9"
 
 
-                console.log(response)
+function getApi() {
+   var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchBarInput.value + "&appid=" + apiKey;
 
-
-            }
-            return response.json()
-        }).then(function (data) {
-            console.log("line 48", data);
-            //need to store lat and lon.
-            var getdatalat
-            var getdatalon
-
-            return data
-        })
-        // } else {
-        //     console.log ("Bad response getting weather data");
-        // }
-        .catch(function (_error) {
-            console.log("Error in getting weather data");
-        })
+   fetch(requestUrl)
+   .then(function (response) {
+    if (response.ok){
+        return response.json()
+    }
+    return null
+})
+   .then(function(data){
+    console.log(data)
+   });
 }
 
-function getCityData(lat, lon) {
-    var date = new Date();
-    var newDate = (date.getMonth() + 1) + "-" + date.getDate() + "-" + date.getFullYear();
-    var weekday = date.toLocaleDateString("en-US", { weekday: 'long' });
-
-    // Use Open Weather API with lattitude and longitude values to pull weather data
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
-    var apiUrlforecast = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
-
-    fetch(apiUrlforecast)
-        .then(function (response) {
-            console.log(response)
-            if (!response.ok) {
-
-                console.log("ok")
-
-                document.getElementById("city").innerHTML = data.name;
-
-                document.getElementById("todays-date").textContent = newDate;
-
-                document.getElementById("temp").textContent = data.main.temp;
-
-                document.getElementById("wind").textContent = data.wind.speed;
-                document.getElementById("humidity").textContent = data.main.humidity;
-            }
-
-        })
-        .catch(function (error) {
-            document.getElementById("warning").style.display = "block";
-            console.log("Error in getting weather data");
-        });
-}
-
-
-
-
-
-
-function getCities() {
-
-    var storedCities = JSON.parse(localStorage.getItem("cityList"));
-
-    if (storedCities) {
-        for (var a = 0; a < storedCities.length; a++) {
-            var cityItem = document.createElement("div");
-            cityItem.className = "prev-city";
-            cityItem.innerHTML = storedCities[a] + "<i class='bi bi-trash' title='Delete city'></i>"
-            historyEl.appendChild(cityItem);
-        }
-
-        cityList = storedCities.slice();
-    }
-}
-
-
-function removeItem(city) {
-    for (var a = 0; a < cityList.length; a++) {
-        if (cityList[a] === city) {
-            cityList.splice(a, 1);
-            console.log(cityList);
-            break;
-        }
-    }
-
-    var historyEl = document.getElementsByClassName("prev-city");
-
-    for (var a = 0; a < historyEl.length; a++) {
-        if (historyEl[a].textContent.indexOf(city) != -1) {
-            historyEl[a].remove();
-        }
-    }
-
-    localStorage.setItem("cityList", JSON.stringify(cityList));
-}
-
-function storeCity(city) {
-    var historyEl = document.getElementById("search-history");
-    alreadyAdded = false;
-
-
-    for (var a = 0; a < cityList.length; a++) {
-        if (cityList[a] === city) {
-            alreadyAdded = true;
-            return;
-        }
-    }
-
-
-    cityList.push(city);
-
-    var cityItem = document.createElement("div");
-    cityItem.className = "prev-city";
-    cityItem.innerHTML = city + "<i class='bi bi-trash' title='Delete city'></i>"
-
-    historyEl.appendChild(cityItem);
-
-    localStorage.setItem("cityList", JSON.stringify(cityList));
-}
-
-// Click event for history sidebar, cities, as well as trash icons
-historyEl.addEventListener("click", function (event) {
-    document.getElementById("city-search").value = "";
-    getCoordinates(event.target.textContent)
-
-    // Clicked on trash icon
-    if (event.target.classList.contains("bi")) {
-        removeItem(event.target.parentNode.textContent);
-    }
-});
+searchBtn.addEventListener("click",getApi);
